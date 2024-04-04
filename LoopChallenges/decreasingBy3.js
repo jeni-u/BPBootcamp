452b34ae7f5eb6ca2b47712259beeaca2c2123ba
jeni-u <jeni000@yahoo.com>
2024-04-04 04:42:25 +0200

    javascript

---
+++ LoopChallenges/decreasingBy3.js
@@ -0,0 +1,6 @@
+//Decreasing multiples of 3//
+for (i=100;i>=0;i--){
+    if(i%3==0){
+        console.log(i)
+    }
+}
---
+++ LoopChallenges/sequence.js
@@ -0,0 +1,4 @@
+var arr = ["4","2","5","1","-0.5","-2","-3.5"]
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
