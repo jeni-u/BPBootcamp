b5b5f6bc181ee6d2f53724af4c3f6b8ffe88c625
jeni-u <jeni000@yahoo.com>
2024-04-14 19:55:00 +0200

    python

---
+++ HelloWorld/hello_world.py
@@ -0,0 +1,16 @@
+print ("Hello World!")
+
+x = "Jeni"
+print("Hello",x,"!")
+print("Hello " + x + "!")
+
+y=13
+print("Hello",y,"!")
+print("Hello " + str(y) + "!")
+
+a = "pizza"
+b = "sushi"
+
+print(f"I love to eat {a} and {b}.")
+c = ("I love to eat {} and {}.").format(a, b)
+print(c)
