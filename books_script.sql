0563210d50346693ba38fcc860390e99e63d48cc
jeni-u <jeni000@yahoo.com>
2024-04-26 23:47:23 +0200

    mySQL

---
+++ books_script.sql
@@ -0,0 +1,66 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema books_schema
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema books_schema
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `books_schema` DEFAULT CHARACTER SET utf8 ;
+USE `books_schema` ;
+
+-- -----------------------------------------------------
+-- Table `books_schema`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `books_schema`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT NOW(),
+  `updated_at` DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `books_schema`.`books`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `books_schema`.`books` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `num_of_pages` INT NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT NOW(),
+  `updated_at` DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `books_schema`.`favorites`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `books_schema`.`favorites` (
+  `user_id` INT NOT NULL,
+  `book_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `book_id`),
+  INDEX `fk_users_has_books_books1_idx` (`book_id` ASC) VISIBLE,
+  INDEX `fk_users_has_books_users_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_users_has_books_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `books_schema`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_books_books1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `books_schema`.`books` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
